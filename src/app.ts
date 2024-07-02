48e612f8a33ce22a89d642952d009a0ed0195ccd
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-07-02 08:01:28 +0100

    chore:  update app ts

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,8 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
+const OPENWEATHERMAP_API_KEY = "039fccb2ae16a8483c796ae5bae7ce96";
+
 const port = 3000 || 8000;
 
 app.get("/", (req: Request, res: Response) => {
@@ -26,13 +28,21 @@ app.get("/api/hello/:visitor_name?", async (req: Request, res: Response) => {
     console.log(visitor_name, "from visitor name");
 
     const response = await axios.get(`https://ipapi.co/${ip}/json/`);
-    const { city } = response.data;
+    const { city, latitude, longitude } = response.data;
+
+    // Get temperature from OpenWeatherMap
+    const weatherResponse = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${OPENWEATHERMAP_API_KEY}`
+    );
+    const temperature = weatherResponse.data.main.temp;
 
     const data = {
       ip,
       location: city,
 
-      greetings: `Hello, ${visitor_name}!, the temperature is 11 degree Celcius in ${city} `,
+      greetings: `Hello, ${visitor_name}!, the temperature is ${
+        temperature || 11.0
+      } degree Celcius in ${city} `,
     };
 
     res.status(200).send(data);
@@ -47,3 +57,4 @@ app.listen(port, () => {
 });
 
 module.exports = app;
+
